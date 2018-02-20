94f4a0611c7589f226455201f06b45c62995b03e
freddygv <vallenilla.fr@gmail.com>
2018-02-20 18:35:24 +0000

    bolt buckets

--- node/node.go
+++ node/node.go
@@ -2,40 +2,108 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/boltdb/bolt"
 	pb "github.com/freddygv/cassandra-wannabe/pb/crud"
 	"google.golang.org/grpc"
 )
 
-const port = ":8081"
+const (
+	port   = ":8081"
+	bucket = "ratings"
+)
 
-type crudServer struct{}
+type crudServer struct {
+	db *bolt.DB
+}
 
 func main() {
+	db, err := bolt.Open("data/node.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatalf("failed to open: %v", err)
+	}
+	initializeDB(db)
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterCRUDServiceServer(s, &crudServer{})
+	pb.RegisterCRUDServiceServer(s, &crudServer{db})
 	s.Serve(lis)
+
+	db.Close()
+}
+
+func initializeDB(db *bolt.DB) {
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(bucket))
+		if err != nil {
+			log.Fatalf("create bucket: %v", err)
+		}
+		return nil
+	})
 }
 
 func (s *crudServer) Read(ctx context.Context, in *pb.Key) (*pb.Record, error) {
-	fmt.Println(in)
-	return &pb.Record{MovieID: in.MovieID, UserID: in.UserID, Rating: 5}, nil
+	var record pb.Record
+
+	err := s.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+
+		key := strconv.Itoa(int(in.UserID)) // TODO: Replace with murmur3
+
+		v := b.Get([]byte(key))
+		err := json.Unmarshal(v, &record)
+		return err
+
+	})
+
+	return &record, err
 }
 
 func (s *crudServer) Upsert(ctx context.Context, in *pb.Record) (*pb.UpsertResponse, error) {
-	fmt.Println(in)
+	err := s.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+
+		key := strconv.Itoa(int(in.UserID)) // TODO: Replace with murmur3
+
+		buf, err := json.Marshal(in)
+		if err != nil {
+			return err
+		}
+
+		err = b.Put([]byte(key), buf)
+		return err
+	})
+	if err != nil {
+		return &pb.UpsertResponse{}, status.Errorf(codes.Internal, "update: %v", err)
+	}
+
 	return &pb.UpsertResponse{}, nil
 }
 
 func (s *crudServer) Delete(ctx context.Context, in *pb.Key) (*pb.DeleteResponse, error) {
-	fmt.Println(in)
+	err := s.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+
+		key := strconv.Itoa(int(in.UserID)) // TODO: Replace with murmur3
+
+		err := b.Delete([]byte(key))
+		return err
+	})
+	if err != nil {
+		return &pb.DeleteResponse{}, status.Errorf(codes.Internal, "delete: %v", err)
+	}
+
 	return &pb.DeleteResponse{}, nil
 }
